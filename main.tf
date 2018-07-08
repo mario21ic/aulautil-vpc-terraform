1469364121eefc3226be4751b1b1a3346fb3de6b
Mario <you@example.com>
2018-07-08 11:12:07 -0500

    Adding elb

--- main.tf
+++ main.tf
@@ -200,3 +200,51 @@ resource "aws_security_group" "sg_private" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
 }
+
+/* ELB */
+resource "aws_security_group" "sg_elb" {
+  name        = "sg_elb_demo"
+  description = "Autoscaling inbound and outbound"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elb" "my_elb" {
+  name               = "elb-demo"
+  # Public subnets
+  subnets         = ["${aws_subnet.sn_public_1.id}", "${aws_subnet.sn_public_2.id}"]
+  security_groups = ["${aws_security_group.sg_elb.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 10
+    unhealthy_threshold = 2
+    timeout             = 5
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+}
